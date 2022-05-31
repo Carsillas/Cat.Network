090c3c84c7a19fc83c9d6830451022f8365dd9ba
burnss9 <burnss9@nku.edu>
2022-05-31 02:13:57 -0400

    osdnfsdnvgudifnvd

--- Cat.Network.Steam/SteamGameClient.cs
+++ Cat.Network.Steam/SteamGameClient.cs
@@ -1,14 +1,70 @@
-﻿using System;
+﻿using Steamworks;
+using Steamworks.Data;
+using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
+using FacepunchClient = Steamworks.SteamClient;
 
 namespace Cat.Network.Steam {
-	public abstract class SteamGameClient : Client, IDisposable {
+	public abstract class SteamGameClient : Client, IConnectionManager, IDisposable {
+		private ConnectionManager ConnectionManager { get; set; }
+		private SteamTransport Transport { get; }
 
-		public SteamGameClient(IProxyManager proxyManager) : base(proxyManager) {
+		public SteamGameClient(SteamGameServer server, IProxyManager proxyManager) : base(proxyManager) {
+			HostTransport clientTransport = new HostTransport();
+			HostTransport serverTransport = new HostTransport();
+
+			clientTransport.Remote = serverTransport;
+			serverTransport.Remote = clientTransport;
+
+			SteamProfileEntity profileEntity = new SteamProfileEntity();
+			profileEntity.Name.Value = FacepunchClient.Name;
+			profileEntity.Id.Value = FacepunchClient.SteamId.Value;
+
+			server.AddTransport(clientTransport, profileEntity);
+			Connect(serverTransport);
+		}
+
+		public SteamGameClient(ulong targetSteamId, IProxyManager proxyManager) : base(proxyManager) {
+			if (FacepunchClient.SteamId != targetSteamId) {
+				Transport = new SteamTransport();
+				ConnectionManager = SteamNetworkingSockets.ConnectRelay<ConnectionManager>(targetSteamId);
+				ConnectionManager.Interface = this;
+			} else {
+				// ???
+			}
+		}
+
+		protected override void PreTick() {
+			base.PreTick();
+
+			ConnectionManager?.Receive();
+		}
+
+		public void Dispose() {
+			ConnectionManager?.Close();
+			ConnectionManager = null;
+		}
+
+		void IConnectionManager.OnConnecting(ConnectionInfo info) {
 
 		}
-		
-		public abstract void Dispose();
+
+		void IConnectionManager.OnConnected(ConnectionInfo info) {
+			Transport.Connection = ConnectionManager.Connection;
+			Connect(Transport);
+		}
+
+		void IConnectionManager.OnDisconnected(ConnectionInfo info) {
+
+		}
+
+		void IConnectionManager.OnMessage(IntPtr data, int size, long messageNum, long recvTime, int channel) {
+			byte[] packet = new byte[size];
+			Marshal.Copy(data, packet, 0, size);
+			Transport.DeliverPacket(packet);
+		}
+
 	}
 }
