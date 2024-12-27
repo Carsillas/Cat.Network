8c2aebea8d6fa9d72ba0c8ef60837ac458a449fc
Carsillas <burnss9@nku.edu>
2024-12-27 17:26:32 -0500

    may have fixed nested properties

--- Cat.Network.Generator/NetworkDataObjectPropertyGenerator.cs
+++ Cat.Network.Generator/NetworkDataObjectPropertyGenerator.cs
@@ -36,28 +36,62 @@ namespace Cat.Network.Generator {
 		protected override void GenerateSetter(ScopedStringWriter writer, int propertyIndex, NetworkPropertyData data) {
 			using (writer.EnterScope("set")) {
 				
+				if (data.TypeInfo.IsNetworkDataObject) {
+					writer.AppendBlock($@"
+						{NetworkDataObjectInterfaceFQN} newValue = value;
+						if (newValue?.Parent != null) {{
+							throw new System.InvalidOperationException($""nameof({NetworkDataObjectFQN})s may only occupy one networked property or list!"");
+						}}
+					");
+				}
+				
 				writer.AppendBlock($@"
-					{NetworkDataObjectInterfaceFQN} iNetworkDataObject = this;
-
+					{NetworkDataObjectInterfaceFQN} iNetworkDataObject = this; // {data.TypeInfo.IsNetworkDataObject}
+					
 					if (iNetworkDataObject.Anchor != null) {{
 						iNetworkDataObject.Anchor.LastDirtyTick = iNetworkDataObject.SerializationContext?.Time ?? 0;
 					}}
 
 					ref {NetworkPropertyInfoFQN} networkPropertyInfo = ref iNetworkDataObject.NetworkProperties[{propertyIndex}];
 					networkPropertyInfo.LastSetTick = iNetworkDataObject.SerializationContext?.Time ?? 0;
-
-					if (iNetworkDataObject.Parent != null) {{
-						if (iNetworkDataObject.Collection == null) {{
-							ref {NetworkPropertyInfoFQN} parentNetworkPropertyInfo = ref iNetworkDataObject.Parent.NetworkProperties[iNetworkDataObject.PropertyIndex];
-							parentNetworkPropertyInfo.LastUpdateTick = iNetworkDataObject.SerializationContext?.Time ?? 0;
+					
+					{NetworkDataObjectInterfaceFQN} current = iNetworkDataObject;
+					{NetworkSerializableInterfaceFQN} parent = iNetworkDataObject.Parent;
+					while (parent != null) {{
+						if (current.Collection == null) {{
+							ref {NetworkPropertyInfoFQN} parentNetworkPropertyInfo = ref parent.NetworkProperties[current.PropertyIndex];
+							parentNetworkPropertyInfo.LastUpdateTick = current.SerializationContext?.Time ?? 0;
 						}} else {{
-							iNetworkDataObject.Collection.MarkForUpdate(iNetworkDataObject.PropertyIndex);
+							current.Collection.MarkForUpdate(current.PropertyIndex);
 						}}
+
+						current = parent as {NetworkDataObjectInterfaceFQN};
+						parent = current?.Parent;
 					}}
 
 					var oldValue = (({NetworkPropertyPrefix})this).{data.Name};
 					(({NetworkPropertyPrefix})this).{data.Name} = value;
 				");
+				
+				if (data.TypeInfo.IsNetworkDataObject) {
+					writer.AppendBlock($@"
+						if (oldValue != null) {{
+							(({NetworkDataObjectInterfaceFQN})oldValue).Parent = null;
+							(({NetworkDataObjectInterfaceFQN})oldValue).PropertyIndex = -1;
+						}}
+						if (newValue != null) {{
+							newValue.Parent = this;
+							newValue.PropertyIndex = {propertyIndex};
+						
+							if (iNetworkDataObject.Anchor?.SerializationContext?.IsDeserializing == false || iNetworkDataObject.Anchor?.SerializationContext?.DeserializeDirtiesProperty == true) {{
+								for (int networkPropertyIndex = 0; networkPropertyIndex < newValue.NetworkProperties.Length; networkPropertyIndex++) {{
+									ref {NetworkPropertyInfoFQN} prop = ref newValue.NetworkProperties[networkPropertyIndex];
+									prop.LastSetTick = iNetworkDataObject.Anchor.SerializationContext.Time;
+								}}
+							}}
+						}}
+					");
+				}
 				
 				if (data.ExposeEvent) {
 					GenerateEventInvocation(writer, propertyIndex, data);
